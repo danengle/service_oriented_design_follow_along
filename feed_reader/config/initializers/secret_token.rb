442e93d8beace777b70eec52d4d4b36e8586a283
Dan Engle <engle.68@gmail.com>
2011-03-19 15:32:00 -0500

    initial commit of feed reader app. start of chapter 3

---
+++ feed_reader/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedReader::Application.config.secret_token = '389e4680b37bbdb7c6d22077435fdf73e74546efc3a29ed9dfc70288c5fb647fa460be87ff4da9d2c2ce5c41e21725b9b1666c2423d47c923fddaa096258bf1a'
